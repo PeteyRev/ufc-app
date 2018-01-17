c7925bec122ef5d4421447502701307b2087895e
Peter Arevalo <pete.g.arevalo@gmail.com>
2018-01-16 22:41:32 -0600

    fixed events page

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <router-link to="/"><img src="./assets/img/ufc-logo.png"></router-link>
       <ul class="main-nav">
         <li><router-link to="title-holders" class="button" active-class="active">Champions</router-link></li>
-        <li><router-link to="fighter-card" class="button" active-class="active">News</router-link></li>
         <li><router-link to="event-listing" class="button" active-class="active">Events</router-link></li>
+        <li><router-link to="fighter-card" class="button" active-class="active">News</router-link></li>
       </ul>
     </header>
     <transition name="fade">
--- src/components/EventListing.vue
+++ src/components/EventListing.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <h2>Upcoming & Past Events</h2>
     <div class="events-wrapper">
-        <div v-for="event in events" class="row collapse">
-            <div class="small-6 columns">
+        <div v-for="event in events" class="event-card" v-bind:data-img="event.feature_image">
+          <div class="event">
+            <div class="event-img">
                 <div class="inner" v-bind:data-link="event.id">
                     <img v-bind:src="event.feature_image" alt="">
                 </div>
             </div>
-            <div class="small-6 columns">
-                <h2>{{event.base_title}}</h2>
-                <p class="location">{{event.location}}</p>
-                <p class="arena">{{event.arena}}</p>
-                <p class="subtitle">{{event.subtitle}}</p>
-                <p class="date">{{event.event_date}} {{event.event_time_zone_text}}{{event.event_time_text}}</p>
-                <a v-bind:href="event.ticketurl" class="button" target="_blank">Buy Tickets</a>
+            <div class="event-info">
+              <div class="inner">
+                <div class="half">
+                  <h2>{{event.base_title}}</h2>
+                  <p class="location">{{event.location}}</p>
+                  <p class="arena">{{event.arena}}</p>
+                  <a v-bind:href="event.ticketurl" class="button" target="_blank">Buy Tickets</a>
+                </div>
+                <div class="half">
+                  <p class="subtitle">{{event.subtitle}}</p>
+                  <p class="date">{{event.event_time_zone_text}}<br>{{event.event_time_text}}</p>
+                </div>
             </div>
+            </div>
+          </div>
         </div>
 	</div>
   </div>
@@ -26,7 +35,7 @@ export default {
   name: "EvenListing",
   data() {
     return {
-      msg: "Upcoming & Past Events",
+      msg: "Events",
       events: {}
     };
   },
